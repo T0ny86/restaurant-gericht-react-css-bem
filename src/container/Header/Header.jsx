d0104b9204f636b7955e0fcf20ffbca71e8aed5a
Tanous Atallah <tanous.atallah@gmail.com>
2022-05-31 22:50:39 +0200

    finish the header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
 import './Header.css';
 
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
+
+
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase the new flavour" />
+      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      <p className='p__opensans margin-2rem' >
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, labore consequatur explicabo eveniet nostrum molestiae perspiciatis minus facere? Voluptatibus quidem minus beatae recusandae numquam quasi officia odit dolore molestias voluptas?
+      </p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="header" />
+    </div>
   </div>
 );
 
