7f94e52520777e5be8d35eb533e2d8f4114b14b6
Tanous Atallah <tanous.atallah@gmail.com>
2022-06-02 20:32:51 +0200

    Adwards section: styled, responsive; add AdwardCard component

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-import './Laurels.css';
+import { SubHeading, AwardCard } from '../../components';
+import { images, data } from '../../constants';
+
 
 const Laurels = () => (
-  <div>
-    Laurels
-  </div>
+    <div className="app__bg app__wrapper section__padding" id="awards">
+        <div className="app__wrapper_info">
+            <SubHeading title="Awards & recognition" />
+            <h1 className="headtext__cormorant">Our Laurels</h1>
+
+            <div className="app__laurels_awards">
+                {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+            </div>
+        </div>
+
+        <div className="app__wrapper_img">
+            <img src={images.laurels} alt="laurels" />
+        </div>
+    </div>
 );
 
 export default Laurels;
